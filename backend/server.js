76a9008fe866c18bbd4938d14ed0d09e3a21b7fb
sajjad heydari <sajjadheydari1401@gmail.com>
2022-02-27 08:11:34 +0330

    Sign up & login api added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
+import User from "./models/userModel.js";
+import generateToken from "./utils/generateToken.js";
 
 colors.enable();
 
@@ -12,8 +14,70 @@ await connectDB();
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 6000;
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+app.post("/api/user", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400).send("User already exists!");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+// @desc    Auth user & get token
+// @route   POST /api/login
+// @access  Public
+app.post("/api/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user.password === password) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401).send("Unauthorized");
+  }
+});
+
+if (process.env.NODE_ENV === "development") {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.underline);
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green
+      .underline
+  );
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,26 @@
-
+import { useEffect } from "react";
+import axios from "axios";
 function App() {
+  const config = {
+    headers: {
+      "Content-Type": "Application/json",
+      Accept: "Application/json",
+    },
+  };
+  useEffect(() => {
+    axios
+      .post(`http://localhost:5000/api/login`, {
+        email: "sajjad@gmail.com",
+        password: "123123",
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Hello Charles</h1>
